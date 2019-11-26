1bf3e84750371f3b1b811d52dc04534bf10376a4
unknown <amansman77@gmail.com>
2019-11-26 17:26:51 +0900

    JpaAuditingConfig 추가

---
+++ src/main/java/com/ho/practice/restapi/config/JpaAuditingConfig.java
@@ -0,0 +1,10 @@
+package com.ho.practice.restapi.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaAuditingConfig {
+
+}
